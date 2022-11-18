262a7202f9652f9958ca4b981dd40056463970d8
Andrés <lopez.andresrigoberto@gmail.com>
2022-11-18 16:11:19 -0600

    update get by user id places

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -25,15 +25,15 @@ const getPlaceById = (req, res, next) => {
   res.json({ place });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => p.creator === userId);
-  if (!place) {
+  const places = DUMMY_PLACES.filter((p) => p.creator === userId);
+  if (!places || places.length === 0) {
     return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
+      new HttpError("Could not find places for the provided user id.", 404)
     );
   }
-  res.json({ place });
+  res.json({ place: places });
 };
 
 const createPlace = (req, res, next) => {
@@ -73,7 +73,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
