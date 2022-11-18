9bfebad671a4c01296211841ca0cc254c04970c9
Andrés <lopez.andresrigoberto@gmail.com>
2022-11-17 20:15:21 -0600

    add controller to add a new place

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,19 +1,19 @@
-//const res = require("express/lib/response");
 const HttpError = require("../models/http-error");
+const { v4: uuidv4 } = require("uuid");
 
 const DUMMY_PLACES = [
-    {
-      id: "p1",
-      title: "Empire State Building",
-      description: "One of the most famous sky scraper in the world",
-      location: {
-        lat: 40.7484474,
-        lng: -73.9871516,
-      },
-      address: "20 W 24th St, New York, NY 10001",
-      creator: "u1",
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scraper in the world",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
     },
-  ];
+    address: "20 W 24th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
 
 const getPlaceById = (req, res, next) => {
   const placeId = req.params.pid;
@@ -36,5 +36,22 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+
+  const createdPlace = {
+    id: uuidv4(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+  res.status(201).json({ place: createdPlace });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
