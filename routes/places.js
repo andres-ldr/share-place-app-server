17c08a06859fc086f38a487094541dedd54ab2ed
Andrés <lopez.andresrigoberto@gmail.com>
2022-12-14 18:31:37 -0600

    add authorization feature

--- routes/places.js
+++ routes/places.js
@@ -2,15 +2,18 @@ const express = require("express");
 const { check } = require("express-validator");
 const placesControllers = require("../controllers/places-controller");
 const fileUpload = require("../middleware/file-upload");
+const checkAuth = require("../middleware/check-auth");
 const router = express.Router();
 
 router.get("/:pid", placesControllers.getPlaceById);
 
 router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
+router.use(checkAuth);
+
 router.post(
   "/",
-  fileUpload.single('image'),
+  fileUpload.single("image"),
   [
     check("title").not().isEmpty(),
     check("description").isLength({ min: 5 }).not().isEmpty(),
