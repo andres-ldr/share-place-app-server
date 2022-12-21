91df4f78f8cef8291fa843dbb1e85fe2e5ae6e62
Andrés <lopez.andresrigoberto@gmail.com>
2022-12-20 18:20:17 -0600

    add response [] when places.length equals 0

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -29,10 +29,10 @@ const getPlaceById = async (req, res, next) => {
 
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  let places;
+  let userWithPlaces;
 
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate("places");
   } catch (err) {
     const error = new HttpError(
       "Could not find a place for the provided user id.",
@@ -41,14 +41,23 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces) {
     return next(
       new HttpError("Could not find places for the provided user id.", 404)
     );
   }
-  res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
-  });
+
+  if (userWithPlaces.places.length === 0) {
+    res.json({
+      places: []
+    });
+  } else {
+    res.json({
+      places: userWithPlaces.places.map((place) =>
+        place.toObject({ getters: true })
+      ),
+    });
+  }
 };
 
 const createPlace = async (req, res, next) => {
